c4423cf3846c92b2cfe2f31e563fec476f6c5838
Ruben Rebelo <rubenrrebelo@users.noreply.github.com>
2021-10-02 19:25:45 +0100

    Add player and fix corners bug

--- Assets/Scripts/RotationTrigger.cs
+++ Assets/Scripts/RotationTrigger.cs
@@ -5,41 +5,89 @@ using UnityEngine;
 public class RotationTrigger : MonoBehaviour
 {
 
-    void OnTriggerStay(Collider other)
+    private GameObject target;
+    private float currentAngle;
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Player")
+    //    {
+    //        PlayerMovement player = other.GetComponent<PlayerMovement>();
+
+    //        print(player.horizontalMove);
+
+    //        Vector3 playerPos = other.transform.position;
+    //        playerPos.y = 0;
+    //        Vector3 triggerPos = transform.position;
+    //        triggerPos.y = 0;
+
+    //        float targetAngle = (player.horizontalMove > 0 ? -90.0f : 90.0f);
+    //        print(targetAngle);
+    //        StartCoroutine(RotatePlayer(other.gameObject, targetAngle));
+
+    //    }
+    //}
+
+    //private IEnumerator RotatePlayer(GameObject player, float targetRotation)
+    //{
+    //    float currentAngle = player.transform.eulerAngles.y;
+    //    float elapsed = 0;
+    //    while(elapsed < 1)
+    //    {
+    //        elapsed += Time.deltaTime / .2f;
+
+    //        player.transform.eulerAngles = new Vector3(0, Mathf.Lerp(currentAngle, currentAngle + targetRotation, elapsed), 0);
+    //        yield return null;
+    //    }
+    //}
+
+    private void OnTriggerStay(Collider other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
-            PlayerMovement player = other.GetComponent<PlayerMovement>();
-            if(player && player.horizontalMove != 0)
+            target = other.gameObject;
+            currentAngle = -Vector3.SignedAngle(transform.right, Vector3.right, Vector3.up);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            target = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            PlayerMovement player = target.GetComponent<PlayerMovement>();
+            float targetAngle = (player.horizontalMove > 0 ? -90.0f : 90.0f);
+
+            Vector3 playerPos = target.transform.position;
+            playerPos.y = 0;
+            Vector3 triggerPos = transform.position;
+            triggerPos.y = 0;
+
+            Vector3 corner = triggerPos + transform.forward - transform.right;
+            float distance = Vector3.Distance(corner, playerPos);
             {
-                
-                Vector3 playerPos = other.transform.position;
-                playerPos.y = 0;
-                Vector3 triggerPos = transform.position;
-                triggerPos.y = 0;
-
-                float distance = Vector3.Distance(triggerPos, playerPos);
-                if(distance > 1)
+                if (targetAngle > 0)
                 {
-                    return;
+                    float angle = (Mathf.Clamp(Vector3.SignedAngle(transform.right.normalized, (playerPos - corner).normalized, Vector3.up), 0, 90));
+                    player.transform.eulerAngles = new Vector3(0, currentAngle + angle, 0);
                 }
-
-
-                float targetAngle = other.transform.rotation.eulerAngles.y + (player.horizontalMove > 0 ? -90.0f : 90.0f);
-
-                //Debug.Log((triggerPos - playerPos).normalized);
-                Debug.Log(distance);
-
-                float angle = Mathf.LerpAngle(targetAngle, other.transform.rotation.eulerAngles.y, distance);
-                if(targetAngle - angle <= targetAngle -  other.transform.rotation.eulerAngles.y)
+                else
                 {
-                    other.transform.RotateAround(other.transform.position, other.transform.up, angle);
+                    float angle = (Mathf.Clamp(Vector3.SignedAngle(transform.right.normalized, (playerPos - corner).normalized, Vector3.up), 0, 90));
+                    player.transform.eulerAngles = new Vector3(0, currentAngle + angle, 0);
+
                 }
-
-                //Debug.Log(Vector3.SignedAngle((triggerPos - playerPos).normalized, transform.forward, Vector3.up));
-                
-
             }
+
+            Vector3 newPos = corner + (playerPos - corner).normalized;
+            player.transform.position = new Vector3(newPos.x, player.transform.position.y, newPos.z);
         }
     }
 }
