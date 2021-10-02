c72715d1673ebcf32fb50f32ee446bc616529fae
davidcalimero <david.d.silva@ist.utl.pt>
2021-10-02 16:31:39 +0200

    player controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,28 +1,117 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController characterController;
+    [SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
+	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
+	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
+	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
+	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
 
-    public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
+	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	public bool m_Grounded;            // Whether or not the player is grounded.
+	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+	private Rigidbody m_Rigidbody;
+	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	private Vector3 m_Velocity = Vector3.zero;
 
-    private Vector3 moveDirection = Vector3.zero;
+    private float horizontalMove;
+    private bool jump = false;
+    public float runSpeed = 40;
 
-    void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-    }
+	[Header("Events")]
+	[Space]
+
+	public UnityEvent OnLandEvent;
+
+	[System.Serializable]
+	public class BoolEvent : UnityEvent<bool> { }
+
+	private void Awake()
+	{
+		m_Rigidbody = GetComponent<Rigidbody>();
+
+		if (OnLandEvent == null)
+			OnLandEvent = new UnityEvent();
+	}
+
+	private void FixedUpdate()
+	{
+        Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+
+		bool wasGrounded = m_Grounded;
+		m_Grounded = false;
+
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
+		Collider[] colliders = Physics.OverlapSphere(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				m_Grounded = true;
+				if (!wasGrounded)
+					OnLandEvent.Invoke();
+			}
+		}
+	}
+
+
+	public void Move(float move, bool jump)
+	{
+        //only control the player if grounded or airControl is turned on
+		if (m_Grounded || m_AirControl)
+		{
+			// Move the character by finding the target velocity
+            Vector3 targetVelocity = transform.forward * move * 10f;
+            targetVelocity.y = m_Rigidbody.velocity.y;
+			// And then smoothing it out and applying it to the character
+			m_Rigidbody.velocity = Vector3.SmoothDamp(m_Rigidbody.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+			// If the input is moving the player right and the player is facing left...
+			if (move > 0 && !m_FacingRight)
+			{
+				// ... flip the player.
+				//Flip();
+			}
+			// Otherwise if the input is moving the player left and the player is facing right...
+			else if (move < 0 && m_FacingRight)
+			{
+				// ... flip the player.
+				//Flip();
+			}
+		}
+		// If the player should jump...
+		if (m_Grounded && jump)
+		{
+			// Add a vertical force to the player.
+			m_Grounded = false;
+			m_Rigidbody.AddForce(new Vector3(0f, m_JumpForce, 0f));
+		}
+	}
+
+
+	/*private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}*/
 
     void Update()
     {
-        moveDirection = transform.forward * Input.GetAxis("Horizontal");
-        moveDirection *= speed;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if (Input.GetButton("Jump"))
+        if(Input.GetButtonDown("Jump"))
         {
-            moveDirection.y = jumpSpeed;
+            jump = true;
         }
 
         if (Input.GetKeyDown("w"))
@@ -34,8 +123,5 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.RotateAround(transform.position, transform.up, 90f);
         }
-
-        moveDirection.y -= gravity * Time.deltaTime;
-        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
